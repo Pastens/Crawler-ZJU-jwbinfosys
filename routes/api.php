db5742865958dfc6f03520fe3726a5bd0951d117
刘昊程 <liuhc@staff.zjutv.com>
2015-10-07 13:02:45 +0800

    first post updated

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,22 @@
 	$app->get('/',function (){
 		echo json_encode('API is go');
 	});
-
 	
+	
+	$app->group('/login',function () use ($app) {
+		$app->post('/',function (){
+			$post_data['UserID'] = $_POST['UserID'];
+			$post_data['UserPassword']   = $_POST['UserPassword'];
+			$post_data['UserType'] = $_POST['UserType'];
+			
+			$cookie = getcookie();
+			$code = verifycode($cookie);
+			
+			if(!$code&&!$cookie){
+				verify($post_data,$cookie);
+			}else{
+				echo json_decode('Initialization Failure');
+			}
+		});
+	});
 ?>
